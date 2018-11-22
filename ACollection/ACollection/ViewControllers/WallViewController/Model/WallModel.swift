71e0eb8590dd6ce7c31b8120c5c0adc04182d3db
Ajumal Ebrahim <ajumal.ebrahim@gmail.com>
2018-11-22 14:40:25 +0300

    Added Needed View and data suporting files

---
+++ ACollection/ACollection/ViewControllers/WallViewController/Model/WallModel.swift
@@ -0,0 +1,9 @@
+//
+//  WallModel.swift
+//  ACollection
+//
+//  Created by Ajumal Ebrahim on 11/22/18.
+//  Copyright © 2018 Ajumal Ebrahim. All rights reserved.
+//
+
+import Foundation
---
+++ ACollection/ACollection/ViewControllers/WallViewController/View/WallViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  WallViewCell.swift
+//  ACollection
+//
+//  Created by Ajumal Ebrahim on 11/22/18.
+//  Copyright © 2018 Ajumal Ebrahim. All rights reserved.
+//
+
+import UIKit
+
+class WallViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
