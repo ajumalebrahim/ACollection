1a059b4f411d0f0c9b7080942f509d729beffddf
Ajumal Ebrahim <ajumal.ebrahim@gmail.com>
2018-11-22 13:12:42 +0300

    Init Commit

---
+++ ACollection/ACollection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ACollection
+//
+//  Created by Ajumal Ebrahim on 11/22/18.
+//  Copyright © 2018 Ajumal Ebrahim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
