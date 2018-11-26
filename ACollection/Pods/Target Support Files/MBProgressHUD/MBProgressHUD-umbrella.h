3805557e45d68c7e801d98952919289a2bf204f8
Ajumal <aebrahim@talabat.com>
2018-11-26 12:21:28 +0300

    Rm ALamofire

--- ACollection/Pods/Target Support Files/MBProgressHUD/MBProgressHUD-umbrella.h
+++ ACollection/Pods/Target Support Files/MBProgressHUD/MBProgressHUD-umbrella.h
@@ -10,7 +10,8 @@
 #endif
 #endif
 
+#import "MBProgressHUD.h"
 
-FOUNDATION_EXPORT double AlamofireVersionNumber;
-FOUNDATION_EXPORT const unsigned char AlamofireVersionString[];
+FOUNDATION_EXPORT double MBProgressHUDVersionNumber;
+FOUNDATION_EXPORT const unsigned char MBProgressHUDVersionString[];
 
